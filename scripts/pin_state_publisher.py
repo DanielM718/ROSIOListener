fa2a7fd3172857611e5d0e864807d11fea58f43d
DanielM718 <danielmejia0405@gmail.com>
2024-02-16 16:07:03 -0600

    io pin publisher node

--- scripts/pin_state_publisher.py
+++ scripts/pin_state_publisher.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
+from std_msgs.msg import Bool
+import RPi.GPIO as GPIO
+
+GPIO_PIN = 16
 
 if __name__ == '__main__':
 	rospy.init_node('pin_state_publisher')
+	
+	pub = rospy.Publisher('button_state', Bool, queue_size = 10)
+
+	#configuring GPIO pin
+	# pull_up_down = GPIO.PUD_UP is push up resistor 
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+	
+	while not rospy.is_shutdown():
+		gpio_state = not GPIO.input(GPIO_PIN)
+		rospy.loginfo(gpio_state)
+		rate.sleep()
+
+	#upon exiting program will clean up all pins used
+	GPIO.cleanup()
